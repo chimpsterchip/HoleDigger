6585e69a9e25b07698de853a6fd4a45ba9d59a1d
Brian <brianpwwong@gmail.com>
2018-06-12 17:18:30 +1200

    Ad and twitter script

---
+++ HoleDigger/Assets/Scripts/SocialMedia/TwitterIntegration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterIntegration : MonoBehaviour {
+
+    [Header("Twitter stuff")]
+    public string url = "http://twitter.com/intent/tweet";
+    public string tweetMessage = "Sup fellas.";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //attach to where twitter is called <- button or something
+    public void OpenTwitter()
+    {
+        Application.OpenURL(url + "?text=" + WWW.EscapeURL(tweetMessage));
+    }
+}
